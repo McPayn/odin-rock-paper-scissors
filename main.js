dac36d1859d4a95f51f206d2df2edb7a59e2fb94
Tyler Brown <brown.tyler.w@gmail.com>
2023-08-20 23:27:37 -0700

    Game now runs with UI, no longer in console

--- main.js
+++ main.js
@@ -3,46 +3,64 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-function playRound(player_choice, computer_choice) {
-    if (player_choice === computer_choice) {
-        return 'It\'s a tie!';
-    }
+function playRound(player_choice) {
+    let computer_choice = getComputerChoice();
+    const result1 = document.getElementById('game-result1');
+    const result2 = document.getElementById('game-result2');
+    result1.innerHTML = 'You: ' + player_choice;
+    result2.innerHTML = 'Computer: ' + computer_choice;
 
-    else if (player_choice === 'rock') {
-        if (computer_choice === 'paper') {
-            return 'You lose!';
-        } else if (computer_choice === 'scissors') {
-            return 'You win!';
+    if (player_choice === 'Rock') {
+        if (computer_choice === 'Paper') {
+            updateScore(0);
+        } else if (computer_choice === 'Scissors') {
+            updateScore(1);
         }
     }
 
-    else if (player_choice === 'paper') {
-        if (computer_choice === 'scissors') {
-            return 'You lose!';
-        } else if (computer_choice === 'rock') {
-            return 'You win!';
+    else if (player_choice === 'Paper') {
+        if (computer_choice === 'Scissors') {
+            updateScore(0);
+        } else if (computer_choice === 'Rock') {
+            updateScore(1);
         }
     }
 
-    else if (player_choice === 'scissors') {
-        if (computer_choice === 'rock') {
-            return 'You lose!';
-        } else if (computer_choice === 'paper') {
-            return 'You win!';
+    else if (player_choice === 'Scissors') {
+        if (computer_choice === 'Rock') {
+            updateScore(0);
+        } else if (computer_choice === 'Paper') {
+            updateScore(1);
         }
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let player_choice = prompt('Rock, paper, or scissors: ');
-        let computer_choice = getComputerChoice().toLowerCase();
-        player_choice = player_choice.toLowerCase();
-        
-        console.log('Computer: ' + computer_choice);
-        console.log('You: ' + player_choice);
-        console.log(playRound(player_choice, computer_choice));
+function updateScore(win) {
+    const playerScore = document.getElementById('scoreboard');
+    const compScore = document.getElementById('comp-scoreboard');
+    let new_player_score = parseInt(playerScore.textContent);
+    let new_comp_score = parseInt(compScore.textContent);
+    if (win) { 
+        new_player_score++;
+        playerScore.innerText = new_player_score;
+        if (new_player_score === 5) { 
+            alert('You win!')
+            reset();
+        }
+    }
+    else { 
+        new_comp_score++;
+        compScore.innerText = new_comp_score; 
+        if (new_comp_score === 5) { 
+            alert('You lose :(')
+            reset();
+        }
     }
 }
 
-game();
+function reset() {
+    document.getElementById('scoreboard').innerText = 0;
+    document.getElementById('comp-scoreboard').innerText = 0;
+    document.getElementById('game-result1').innerHTML = 'Welcome to Rock Paper Scissors!';
+    document.getElementById('game-result2').innerHTML = '';
+}
